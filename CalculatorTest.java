aecd82e3cbdd71860b9dc57a83a01237607af933
mca <mca@L1-56.ssccdomain.in>
2025-08-29 14:25:04 +0530

    Initial Commit

---
+++ CalculatorTest.java
@@ -0,0 +1,9 @@
+CalculatorTest.java;
+import org.junit.test;
+import staticorg junit.assest.assert Equal;
+public class calculatortest{
+calculator calc=new calculator();
+@test public void test addition () {assestEqual(15,calc.add(10,5));}
+@test public void test subtraction() {assestEqual(5,calc.subtract(10,5));}
+@test public void test multiplication() {assestEqual(5,calc.multiply(10,5));}
+@test public void test division() {assestEqual(2,calc.division(10,5));}
