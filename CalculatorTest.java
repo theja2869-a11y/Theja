ab96b937daeb3f74d1b97e6f04e9e5db8abc30ba
theja2869-a11y <theja2869@gmail.com>
2025-08-29 15:11:43 +0530

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,9 +1,26 @@
-CalculatorTest.java;
-import org.junit.test;
-import staticorg junit.assest.assert Equal;
-public class calculatortest{
-calculator calc=new calculator();
-@test public void test addition () {assestEqual(15,calc.add(10,5));}
-@test public void test subtraction() {assestEqual(5,calc.subtract(10,5));}
-@test public void test multiplication() {assestEqual(5,calc.multiply(10,5));}
-@test public void test division() {assestEqual(2,calc.division(10,5));}
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAddition() {
+        assertEquals(15, calc.add(10, 5));
+    }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(5, calc.subtract(10, 5));
+    }
+
+    @Test
+    public void testMultiplication() {
+        assertEquals(50, calc.multiply(10, 5));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals(2, calc.division(10, 5));
+    }
+}
